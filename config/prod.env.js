15aedcf32ac4ff4d68a1297ee148f43614bb0411
brianchitester <yagoogaly@gmail.com>
2018-11-04 00:47:51 -0400

    config

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,10 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  apiKey: JSON.stringify(process.env.apiKey),
+  authDomain: JSON.stringify(process.env.authDomain),
+  databaseURL: JSON.stringify(process.env.databaseURL),
+  projectId: JSON.stringify(process.env.projectId),
+  storageBucket: JSON.stringify(process.env.storageBucket),
+  messagingSenderId: JSON.stringify(process.env.messagingSenderId)
 }
