14a99b4ed3cd60c44337963cc69d84eefd06b4c1
brianchitester <yagoogaly@gmail.com>
2018-11-04 13:25:52 -0500

    oops

--- src/main.js
+++ src/main.js
@@ -8,13 +8,13 @@ import firebase from 'firebase'
 Vue.config.productionTip = false
 
 let app;
-let config = {
-  VUE_APP_API_KEY: process.env.VUE_APP_API_KEY,
-  VUE_APP_AUTH_DOMAIN: process.env.VUE_APP_AUTH_DOMAIN ,
-  VUE_APP_DATABASE_URL: process.env.VUE_APP_DATABASE_URL ,
-  VUE_APP_PROJECT_ID: process.env.VUE_APP_PROJECT_ID ,
-  VUE_APP_STORAGE_BUCKET: process.env.VUE_APP_STORAGE_BUCKET ,
-  VUE_APP_MESSAGING_SENDER_ID: process.env.VUE_APP_MESSAGING_SENDER_ID
+const config = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN ,
+  databaseURL: process.env.VUE_APP_DATABASE_URL ,
+  projectId: process.env.VUE_APP_PROJECT_ID ,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET ,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config)
